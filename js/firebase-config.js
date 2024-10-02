ff3ae113e03fd1a94f2a3f425ab78ea6331ba51f
starrvarse <starrvarse@gmail.com>
2024-10-02 17:42:57 +0530

    firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,12 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional 
 const firebaseConfig = {
-    apiKey: "AIzaSyAyX9Oad7N8AxizdtK7MvMFkK0HIiE1oXQ",
-    authDomain: "bankingapp-52159.firebaseapp.com",
-    projectId: "bankingapp-52159",
-    storageBucket: "bankingapp-52159.appspot.com",
-    messagingSenderId: "807339322620",
-    appId: "1:807339322620:web:a9405840bac480f6fb3b79",
-    measurementId: "G-HY69V9T1MM"
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxx",
+    projectId: "xxxxxxxxxxxxxxxxxxxxxxxxx",
+    storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxx",
+    appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    measurementId: "xxxxxxxxxxxxxxxxxxxxx"
   };
   
  // Initialize Firebase
