d61a25cac9badb2bddfcb233bbbac1e09591a278
starrvarse <starrvarse@gmail.com>
2024-10-02 17:43:34 +0530

    email.js

--- js/email.js
+++ js/email.js
@@ -6,11 +6,11 @@ document.addEventListener("DOMContentLoaded", function() {
         const subject = document.getElementById("subject").value;
         const message = document.getElementById("message").value;
 
-        const apiKey = "1333A7D9D23A654762A151C5D7A9952B9B3243A70A44B51C722112B54FA028EB966E1DD7E5C5D6014D42860B9697AB92";  // Replace with your Elastic Email API key
+        const apiKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";  // Replace with your Elastic Email API key
 
         const emailData = {
             apikey: apiKey,
-            from: "statement@nbisolutions.xyz",  // Your sender email
+            from: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",  // Your sender email
             to: toEmail,
             subject: subject,
             bodyText: message,
